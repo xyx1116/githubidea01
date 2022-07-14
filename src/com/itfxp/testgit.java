28f2b1d19420dfcfabfd688cf4e1ea1ee978818e
xyx <1074911434@qq.com>
2022-07-14 17:55:03 +0800

    第一次提交testgit文件

--- src/com/itfxp/testgit.java
+++ src/com/itfxp/testgit.java
@@ -2,6 +2,6 @@ package com.itfxp;
 
 public class testgit {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello different git");
     }
 }
